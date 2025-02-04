bd23f6883294e07dc8b57ee80f424826340e0f65
ctietz2 <ctietz2@huskers.unl.edu>
2025-02-03 18:03:31 -0600

    Improved reset button
    Reset button rights the buggy, but the buggy's animations aren't working.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] private float speed = 5.0f;
+    private float speed = 10.0f;
     [SerializeField] private GameObject projectilePrefab;
 
     // Start is called before the first frame update
@@ -21,7 +21,7 @@ public class Projectile : MonoBehaviour
         {
             prefabInstance = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
             Rigidbody rb = prefabInstance.GetComponent<Rigidbody>();
-            rb.AddForce(Vector3.forward * speed, ForceMode.Impulse);
+            rb.AddForce(transform.forward * speed, ForceMode.Impulse);
         }
 
 /*        transform.Rotate(Vector3.forward * speed * Time.deltaTime);*/
